1e4bd73c7c0582abba6fd6b622e99be470da488e
ttdangg <ttdang1445@gmail.com>
2024-11-12 10:17:41 -0800

    problem 3

--- Main.java
+++ Main.java
@@ -4,14 +4,67 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // You can test your method here
-    // System.out.println(isPrime(1));
-    // System.out.println(isPrime(17));
+    Scanner sc = new Scanner(System.in);
+
+    // Problem 1
+
+    // System.out.println("Input String: ");
+    // String input = sc.nextLine();
+    // int count = 0;
+    // for (int i = 0; i<input.length()-1; i++)
+    // {
+    //   String vowel_pair = input.substring(i,i+2);
+    //   if (vowel_pair.equals("pa")||vowel_pair.equals("pe")||vowel_pair.equals("pi")||vowel_pair.equals("po")||vowel_pair.equals("pu"))
+    //   {
+    //     count ++;
+    //   }
+
+    // }
+    // System.out.println("Contains p followed by a vowel " + count + " times");
+
+
+    // Problem 2
+
+    // String word = "peter piper picked a pack of pickle peppers";
+    // String new_word = "";
+    // for (int i = 0; i<word.length(); i++)
+    // {
+    //   String letter = word.substring(i, i+1);
+    //   if (!(letter.equals("e") || letter.equals("t") || letter.equals("a") || letter.equals("i") || letter.equals("o")))
+    //   {
+    //     new_word += letter;
+    //   }
+    // }
+    // System.out.println(new_word);
+
+
+
+
+    // Problem 3
+    String string1; 
+    String string2; 
+    String output = "";
+
+    if (string2.length()==string1.length())
+    {
+      for (int i = string1.length()-1; i>=0; i-- )
+      {
+        
+      }
+
+    }
+    else
+    {
+      System.out.print("error");
+    }
+   
+
+
+
+
+
+
   }
 
-  public static boolean isPrime(int N)
-  {
-    // TODO: Replace and write your code below
-    return false;
-  }
+  
 }
